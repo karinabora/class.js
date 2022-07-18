e0499bfa55107be2c1c3e45b1e09e0c874db6cdc
Karina Bora <96630595+karinabora@users.noreply.github.com>
2022-07-18 12:40:37 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,33 @@
+// Classes are a template for creating objects.
+
+class Mobile{
+    constructor(){
+        this.a=2,
+        this.b=3,
+        this.d=4,
+        this.e=5
+    }
+}
+c=new Mobile
+console.log(c);
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class a{
+  constructor (){
+    this.a=2,
+    this.b=4
+  }
+}
+c=new a()
+console.log(c);
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = 2;
+    this.width = 4;
+ 
+ }
+}
+a=new Rectangle()
+console.log(a);
